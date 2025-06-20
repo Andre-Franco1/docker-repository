81c4b2a17542326a9ccac4f27527c59683c64462
Andre-Franco1 <andrefranco3172@gmail.com>
2025-06-20 12:19:29 -0300

    adding docker study cases

---
+++ docker/Python/events.py
@@ -0,0 +1,9 @@
+import docker
+
+client = docker.DockerClient(base_url='tcp://127.0.0.1:2375')
+
+for event in client.events(decode=True, filters={"event": "die"}):
+    container_id = event["id"]
+    container_name = event["Actor"]["Attributes"]["name"]
+    epoch_time = event["time"]
+    print("O container %s (%s) foi finalizado às %s" % (container_name, container_id, epoch_time))
