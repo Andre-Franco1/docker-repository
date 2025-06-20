dd3fea650a16155b20d032da5c52973a2e8423b6
Andre-Franco1 <andrefranco3172@gmail.com>
2025-06-20 12:25:15 -0300

    added convert epoch time to date time

--- docker/Python/events.py
+++ docker/Python/events.py
@@ -1,4 +1,5 @@
-import docker
+import docker 
+import datetime
 
 client = docker.DockerClient(base_url='tcp://127.0.0.1:2375')
 
@@ -6,4 +7,5 @@ for event in client.events(decode=True, filters={"event": "die"}):
     container_id = event["id"]
     container_name = event["Actor"]["Attributes"]["name"]
     epoch_time = event["time"]
-    print("O container %s (%s) foi finalizado às %s" % (container_name, container_id, epoch_time))
+    date_time = datetime.datetime.fromtimestamp(epoch_time)
+    print("O container %s (%s) foi finalizado às %s" % (container_name, container_id, date_time))
